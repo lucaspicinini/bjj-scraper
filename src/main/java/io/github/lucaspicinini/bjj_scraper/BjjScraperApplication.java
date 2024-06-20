5cb556ab203c1a3799c3fc460ec955b24b30915b
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-06-20 15:52:56 -0300

    add repository and main app controller in the application entry point

--- src/main/java/io/github/lucaspicinini/bjj_scraper/BjjScraperApplication.java
+++ src/main/java/io/github/lucaspicinini/bjj_scraper/BjjScraperApplication.java
@@ -1,13 +1,25 @@
 package io.github.lucaspicinini.bjj_scraper;
 
+import io.github.lucaspicinini.bjj_scraper.controller.AllFightersRequest;
+import io.github.lucaspicinini.bjj_scraper.repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BjjScraperApplication {
+public class BjjScraperApplication implements CommandLineRunner {
+	@Autowired
+	private Repository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BjjScraperApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		AllFightersRequest.getAllFighters(repository);
+		System.out.println("Scrap done!");
 	}
 
 }
