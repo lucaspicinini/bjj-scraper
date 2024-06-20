1e1edd190e07b4280bd2d154da29ab9e966e63e5
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-06-20 13:42:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bjj-scraper
+ Scraper to get data of BJJ athletes over the world.
---
+++ src/test/java/io/github/lucaspicinini/bjj_scraper/BjjScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucaspicinini.bjj_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjjScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
