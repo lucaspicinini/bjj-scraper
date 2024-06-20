00c26d9167688007798b4e240437c1e1bd320e77
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-06-20 19:45:10 -0300

    finishes main controller implementation

--- src/main/java/io/github/lucaspicinini/bjj_scraper/controller/AllFightersRequest.java
+++ src/main/java/io/github/lucaspicinini/bjj_scraper/controller/AllFightersRequest.java
@@ -1,11 +1,16 @@
 package io.github.lucaspicinini.bjj_scraper.controller;
 
 import io.github.lucaspicinini.bjj_scraper.model.dto.FighterDTO;
+import io.github.lucaspicinini.bjj_scraper.model.entity.Fighter;
+import io.github.lucaspicinini.bjj_scraper.model.entity.Team;
 import io.github.lucaspicinini.bjj_scraper.repository.Repository;
 import io.github.lucaspicinini.bjj_scraper.service.FighterRequest;
 import io.github.lucaspicinini.bjj_scraper.service.HomeRequest;
+import io.github.lucaspicinini.bjj_scraper.utils.AiApiConfigs;
+import io.github.lucaspicinini.bjj_scraper.utils.FighterTranslator;
 
 import java.util.List;
+import java.util.Optional;
 
 public class AllFightersRequest {
 
@@ -16,6 +21,32 @@ public class AllFightersRequest {
         for (var fighterHref : fightersHrefs) {
             String fighterResponseBody = FighterRequest.getResponseBody(fighterHref);
             FighterDTO fighterDTO = FighterScraper.scrap(fighterResponseBody, fighterHref);
+            Fighter fighter = new Fighter(fighterDTO);
+            Team team;
+
+            try {
+                Optional<Team> teamSearch = repository.findByName(fighterDTO.team().name());
+
+                if (teamSearch.isPresent()) {
+                    fighter.setTeam(teamSearch.get());
+                    teamSearch.get().setFighters(fighter);
+                    team = teamSearch.get();
+                } else {
+                    Team newTeam = new Team(fighterDTO.team());
+                    newTeam.setFighters(fighter);
+                    fighter.setTeam(newTeam);
+                    team = newTeam;
+                }
+
+                if (AiApiConfigs.USE_AI_API) {
+                    FighterTranslator.translateWithAi(fighter);
+                }
+
+                repository.save(team);
+                System.out.println("Atleta " + fighter.getFullName() + " criado com sucesso!");
+            } catch (Exception e) {
+                System.out.println("O atleta " + fighter.getFullName() + " já foi armazenado anteriormente");
+            }
         }
     }
 }
