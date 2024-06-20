d5bead0a46271759647bc10a454b5949bc062a33
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-06-20 19:08:04 -0300

    Implements Team Entity

--- src/main/java/io/github/lucaspicinini/bjj_scraper/model/entity/Team.java
+++ src/main/java/io/github/lucaspicinini/bjj_scraper/model/entity/Team.java
@@ -1,4 +1,32 @@
 package io.github.lucaspicinini.bjj_scraper.model.entity;
 
+import io.github.lucaspicinini.bjj_scraper.model.dto.TeamDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "equipes")
+@NoArgsConstructor
+@Getter
+@ToString
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<Fighter> fighters = new ArrayList<>();
+
+    public Team(TeamDTO team) {
+        this.name = team.name();
+    }
+
+    public void setFighters(Fighter fighter) {
+        this.fighters.add(fighter);
+    }
 }
