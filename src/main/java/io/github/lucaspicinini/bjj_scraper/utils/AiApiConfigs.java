512d6f4b07d27f2a645b93757f36f2d159a35b4d
Lucas Picinini <137008535+lucaspicinini@users.noreply.github.com>
2024-06-20 19:44:40 -0300

    Implements configs for AI Mistral API.

---
+++ src/main/java/io/github/lucaspicinini/bjj_scraper/utils/AiApiConfigs.java
@@ -0,0 +1,6 @@
+package io.github.lucaspicinini.bjj_scraper.utils;
+
+public class AiApiConfigs {
+    public static final boolean USE_AI_API = false;
+    public static final String MISTRAL_AI_KEY = System.getenv("MISTRAL_AI_KEY");
+}
